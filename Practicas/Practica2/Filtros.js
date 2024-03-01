95612e65b816e751b49c06515d56b7e39e508a80
Denys Litvynov <denislit741@gmail.com>
2024-03-01 14:24:04 +0100

    Archivos nuevos creados, prac2

---
+++ Practicas/Practica2/Filtros.js
@@ -0,0 +1,50 @@
+//
+// Archivo: Filtros.js
+// Descripción: Implementa la función filtrar(). Pruébala de forma automática 
+// seleccionando de una lista de palabras, aquéllas que contengan la letra ’x’.
+// Fecha: 01/03/2024 
+// Nombre: Denys Litvynov
+//
+
+//
+// Lista<T> (T -> V/F) -> filtrar() -> Lista<T>
+//
+function filtrar(array,condicion){
+    const resultado = [];
+    for(let i = 0; i < array.length; i++){
+        if(condicion(array,i)){
+            resultado.push(array[i]);
+        }
+    }// for
+    return resultado;
+}// ()
+
+//--------------------------------------------
+//--------------------------------------------
+
+// Prueba automática para la función 
+function probarFiltrar(){
+    const array = ["Elixir","Sintaxis","Hola","Cinco","Matrix"];
+    const res = filtrar(array, function(arr,i){
+        if(arr[i].includes(x)){
+            return true;
+        }
+        return false;
+    });
+    const arrayPrueba = ["Elixir","Sintaxis","Matrix"];
+    for(let i = 0; i<res.length; i++){
+        if(res[i]!=arrayPrueba[i]){
+            console.log("Esta mal");
+        }
+    }// for
+}// ()
+
+//--------------------------------------------
+// main()
+//--------------------------------------------
+
+probarFiltrar();
+
+//--------------------------------------------
+//--------------------------------------------
+//--------------------------------------------
